9f5bd07cf9875905850ffba33a43313383fee985
Jigme Topgyal <143983524+JigmeT04@users.noreply.github.com>
2025-03-17 13:06:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Import the Express module
+/*// Import the Express module
 const express = require('express');
 
 // Create an instance of Express
@@ -16,3 +16,15 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`The server is running on http://localhost:${PORT}`);
 });
+*/
+const express = require('express')
+const app = express()
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
